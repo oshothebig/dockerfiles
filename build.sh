54e4d47522ca5d564b7cb75f7c6dd54674916b55
Sho SHIMIZU <sho.shimizu@gmail.com>
2021-03-13 17:19:44 +0900

    Skip building Docker image when Dockerfile doesn't exist

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ set -euo pipefail
 
 for image in $(ls -d */ | sed -e 's|/||')
 do
+    if [[ ! -f ${image}/Dockerfile ]]; then
+        continue
+    fi
+
     docker build -f ${image}/Dockerfile -t ${image} ${image}
 done
